d103de75b7362e8ced6a385fb4e67b53a71383c5
maxmaster <yousseflkashef000@gmail.com>
2023-08-26 03:42:24 +0300

    part 2 is done

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,14 +7,13 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
+KERNEL_VERSION=v6.1.14
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-if [ $# -lt 1 ]
-then
+if [ $# -lt 1 ]; then
 	echo "Using default directory ${OUTDIR} for output"
 else
 	OUTDIR=$1
@@ -25,56 +24,100 @@ mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
+	#Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+	cd linux-stable
+	echo "Checking out version ${KERNEL_VERSION}"
+	git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+	# TODO: Add your kernel build steps here
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+	make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+	make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
+if [ -d "${OUTDIR}/rootfs" ]; then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+	sudo rm -rf ${OUTDIR}/rootfs
 fi
 
 # TODO: Create necessary base directories
 
+mkdir "$OUTDIR/rootfs"
+cd "$OUTDIR/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/sbin usr/lib
+mkdir -p var/log
+
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+if [ ! -d "${OUTDIR}/busybox" ]; then
+	git clone git://busybox.net/busybox.git
+	cd busybox
+	git checkout ${BUSYBOX_VERSION}
+	# TODO:  Configure busybox
+	make distclean
+	make defconfig
+
 else
-    cd busybox
+	cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo "Copy dependencies in gcc folder to rootfs"
 
+cp -L ${FINDER_APP_DIR}/necessary_lib/lib64/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+cp -L ${FINDER_APP_DIR}/necessary_lib/lib64/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib64/
+cp -L ${FINDER_APP_DIR}/necessary_lib/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp -L ${FINDER_APP_DIR}/necessary_lib/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+cp -L ${FINDER_APP_DIR}/necessary_lib/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 # TODO: Make device nodes
-
+sudo mknod -m 666 dev/null c 1 3    #redirect either error or output of a command to this file
+sudo mknod -m 600 dev/console c 5 1 #show messages during startup and shutdown
 # TODO: Clean and build the writer utility
+echo "Jump to folder finder-app: ${FINDER_APP_DIR}"
+cd ${FINDER_APP_DIR}
+CROSS_COMPILE=aarch64-none-linux-gnu- make clean
+CROSS_COMPILE=aarch64-none-linux-gnu- make
+echo "Make binary writer done"
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -r ./conf/ ${OUTDIR}/rootfs/
+cp -r -n ./* ${OUTDIR}/rootfs/home/
+cp --parents conf/username.txt conf/assignment.txt ${OUTDIR}/rootfs/home/
+echo "Copy all files done :) "
 
 # TODO: Chown the root directory
-
+cd ${OUTDIR}/rootfs/
+sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+if [ -f initramfs.cpio.gz ]; then
+	rm -f initramfs.cpio.gz
+fi
+
+cd $OUTDIR/rootfs
+rm -rf ../initramfs.cpio*
+find . | cpio -H newc -ov --owner root:root >../initramfs.cpio
+cd ..
+gzip -f initramfs.cpio
+
+cd "$OUTDIR"
+
+# mkimage -A arm64 -O linux -T ramdisk -C gzip -d initramfs.cpio.gz initramfs
